357267f96940aace0512f1f85ed522621f5080c5
osypenkopavel <74303823+osypenkopavel@users.noreply.github.com>
2022-10-24 18:12:37 +0300

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,26 @@
+﻿var path = "TextFile1.csv";
+var phoneBook = ReadFile(path);
+var output = SearchCustomer(Console.ReadLine(), phoneBook);
+Console.WriteLine(string.Join("\r\n", output));
+
+
+static List<(string Id, string FirstName, string LastName, string PhoneNumber)> ReadFile(string path)
+    {
+        var phoneBook = new List<(string Id, string FirstName, string LastName, string PhoneNumber)> ();
+        var textFile = File.ReadAllLines(path);
+        foreach (var s in textFile)
+        {
+            var separate = s.Split(",");
+            phoneBook.Add((separate[0], separate[1], separate[2], separate[3]));
+        }
+        return phoneBook;
+    }
+
+(string, string, string, string)[] SearchCustomer(
+    string input,
+    List<(string, string, string, string)> collection) =>
+    collection.Where(person =>
+    person.Item1.Contains(input) ||
+    person.Item2.Contains(input, StringComparison.OrdinalIgnoreCase) ||
+    person.Item3.Contains(input, StringComparison.OrdinalIgnoreCase) ||
+    person.Item4.Contains(input)).ToArray();
